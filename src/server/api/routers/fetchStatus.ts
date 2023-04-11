97f7f5f39ab89c7d03afc61fef8410bef1daf174
xavier-kong <kongxianchengxavier@gmail.com>
2023-04-11 18:16:57 +0800

    airport code

--- src/server/api/routers/fetchStatus.ts
+++ src/server/api/routers/fetchStatus.ts
@@ -5,22 +5,34 @@ import { ddbClient } from "~/utils/dynamoDbConfig"
 import { DynamoDBClient, GetItemCommand, GetItemCommandInput } from "@aws-sdk/client-dynamodb";
 import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
 
+function createDateString(): string {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = (today.getMonth() + 1).toString().padStart(2, '0');
+    const day = today.getDate().toString().padStart(2, '0');
+    return `${year}-${month}-${day}`;
+}
+
 const queryDb = async () => {
     if (!process.env.TABLE_NAME) {
         console.log("no table name configured in env");
         return;
     }
 
+    const dateString = createDateString();
+
     const params: GetItemCommandInput = {
         TableName: process.env.TABLE_NAME,
         Key: marshall({
-            "dateString":
+            "dateString": dateString
         }),
     }
 
     const res = await ddbClient.send(new GetItemCommand(params));
     if (res.Item) {
-
+        return true;
+    } else {
+        return false;
     }
 }
 
